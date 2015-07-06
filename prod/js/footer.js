e6bcbe1f7622df3a42c0983598c74ccf8d11c028
bryantaylor <bryant486@gmail.com>
2015-07-06 18:44:54 -0400

    implemented header

--- prod/js/footer.js
+++ prod/js/footer.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
 
+	function stickyHeader() {
+		var bodyContentTop = $('.body_content').offset().top;
+
+		$(window).scroll(function() {
+			if ($(window).scrollTop() > bodyContentTop) {
+				$('.head_primary').addClass('fixed_header');
+				console.log('fixxxed');
+			} else {
+				$('.head_primary').removeClass('fixed_header');
+				console.log('Removed fixed')
+			}
+		});
+
+	}
+	stickyHeader();
+
+	function menuButton() {
+		
+	}
+
+
 });
